b1019fbc6ff5f146bd4f940d7ad0bf71f0a75b34
dbalstjr16 <148639968+dbalstjr16@users.noreply.github.com>
2025-03-23 13:39:44 -0500

    implemented client side register page

--- my-fullstack-app/client/src/components/Register.tsx
+++ my-fullstack-app/client/src/components/Register.tsx
@@ -1,16 +1,66 @@
 import { Form, Button } from 'react-bootstrap';
+import { useRef } from 'react';
+import { useNavigate  } from 'react-router-dom';
 
-function Register(props: any) {
+function Register() {
     
+    const userIDRef = useRef<any>(null)
+    const passwordRef = useRef<any>(null);
+    const cPasswordRef = useRef<any>(null);
+
+    const navigate = useNavigate();
+
+    function handleRegister() {
+        const userID = userIDRef.current.value;
+        const password = passwordRef.current.value;
+        const cPassword = cPasswordRef.current.value;
+
+        if (!userID || !password || !cPassword) {
+            alert('All Fields should not be empty!');
+            return;
+        }
+        if (password !== cPassword) {
+            alert('Password and confirm password do not match, please try again.');
+            return;
+        }
+
+        fetch("http://localhost:3000/users/register", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                userID: userID,
+                password: password
+            }),
+            credentials: "include"
+        })
+        .then(res => {
+            if (res.ok) { return res.json(); }
+            else { return res.json().then(data => { throw new Error(data.error) }); }
+        })
+        .then(data => {
+            alert(data.message);
+            navigate("/");
+        })
+        .catch(error => {
+            alert(error);
+        })
+    }
+
     return <>
         <h3>Register your account!</h3>
-        <Form.Label>userID</Form.Label>
-        <Form.Control></Form.Control>
-        <Form.Label>Password</Form.Label>
-        <Form.Control></Form.Control>
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control></Form.Control>
-        <Button style={{ marginTop: 10 }}>Register</Button>
+
+        <Form.Label htmlFor="userID">userID</Form.Label>
+        <Form.Control id="userID" ref={userIDRef}></Form.Control>
+
+        <Form.Label htmlFor="password">Password</Form.Label>
+        <Form.Control id="password" type="password" ref={passwordRef}></Form.Control>
+
+        <Form.Label htmlFor="cPassword">Confirm Password</Form.Label>
+        <Form.Control id="cPassword" type="password" ref={cPasswordRef}></Form.Control>
+
+        <Button style={{ marginTop: 10 }} onClick={() => handleRegister()}>Register</Button>
     </>;
 }
 
